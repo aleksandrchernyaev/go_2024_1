ad5e48f18f3f732386b50b067cc63dd1b5468d46
Cherniaiev Aleksandr <pifago@ukr.net>
2024-06-11 19:07:20 +0300

    1

---
+++ hw-2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	var txt string
+	var low_word string
+	dictionary := map[string]int{}
+
+	type key_value struct {
+		key   string
+		value int
+	}
+
+	txt = `This string lines is on
+	multiple lines
+	within three single
+	quotes on either lines side.`
+
+	splitFunc := func(r rune) bool {
+		return strings.ContainsRune("*%,.!_ ;:\n-?	", r)
+	}
+
+	words := strings.FieldsFunc(txt, splitFunc)
+	for _, word := range words {
+		low_word = strings.ToLower(word)
+		//fmt.Printf("Word %d is: %s \n", idx, low_word)
+
+		if _, ok := dictionary[low_word]; !ok {
+			dictionary[low_word] = 0
+		}
+		dictionary[low_word] = dictionary[low_word] + 1
+		//sorted_struct = append(sorted_struct , key_value {key, value})
+	}
+	//var slice_struct []key_value
+
+	slice := make([]key_value, len(dictionary)+1)
+
+	for key, value := range dictionary {
+		slice = append(slice, key_value{key, value})
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		return slice[i].value > slice[j].value
+	})
+
+	for _, key_value := range slice {
+		fmt.Printf("%s, %d !!!\n", key_value.key, key_value.value)
+	}
+}
